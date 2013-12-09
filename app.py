bb7271133e274aca7312c12143224998d2ff8d45
Scott Simpson <sasimpson@gmail.com>
2013-12-09 15:43:09 -0600

    adding a bunch of stuff and functionality

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from flask import Flask
+import re
+from flask import Flask, render_template, jsonify, abort
+from requests import get
+from bs4 import BeautifulSoup
 
 class Config(object):
     DEBUG = True
@@ -10,7 +13,20 @@ app.config.from_object(Config())
 
 @app.route('/')
 def index():
-    return "works"
+    req = get('http://www.qrz.com')
+    if req.ok:
+        soup = BeautifulSoup(req.text)
+        swlist = soup.find('div', {'class': 'swlist'})
+        links = swlist.findAll('a', {'class': 'sl'})
+        data = []
+        for link in links:
+            if not re.search('wanted', link['title'], re.I) \
+                and not re.search('wtb', link['title'], re.I) \
+                and not re.search('want to buy', link['title'], re.I):
+                data.append((link['title'], link['href']))
+        return render_template('index.html', data=data)
+    else:
+        abort(500)
 
 if __name__ == "__main__":
     app.run('0.0.0.0')
