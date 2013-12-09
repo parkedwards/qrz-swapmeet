adf3774bae5e7ed5e0e9187a6936c47069db7764
Scott Simpson <sasimpson@gmail.com>
2013-12-09 12:58:20 -0600

    adding project

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+def Config(object):
+    DEBUG = True
+    HOST = '0.0.0.0'
+    PORT = '5000'
+
+app = Flask(__name__)
+app.config.from_object(Config())
+
+@app.route('/')
+def index():
+    return "works"
+
+if __name__ == "__main__":
+    app.run()
